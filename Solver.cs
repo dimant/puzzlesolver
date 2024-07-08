39ae286ec9c89047d234cfba256f0bad0ef70758
Diman Todorov <diman.todorov@outlook.com>
2024-07-08 02:45:44 +0000

    solver

---
+++ Solver.cs
@@ -0,0 +1,40 @@
+class Solver
+{
+    public Solution? Solve(PuzzlePiece[] pieces)
+    {
+        var solution = new Solution();
+
+        if (Solve(solution, pieces, 0))
+        {
+            return solution;
+        }
+
+        return null;
+    }
+
+    private bool Solve(Solution solution, PuzzlePiece[] pieces, int index)
+    {
+        if (index == pieces.Length)
+        {
+            return true;
+        }
+
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                if (solution.TryAddPiece(pieces[index], row, col))
+                {
+                    if (Solve(solution, pieces, index + 1))
+                    {
+                        return true;
+                    }
+
+                    solution.RemoveLast();
+                }
+            }
+        }
+
+        return false;
+    }
+}
